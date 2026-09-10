d94527ab019e1e9c660548e4fa782059e0b924dc
Brandon Kounse <kounse@gmail.com>
2026-09-10 16:56:32 -0700

    added closure to pass in db injection, moved db out of handler

--- handler.go
+++ handler.go
@@ -1,26 +1,36 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"net/http"
-	"time"
+
+	"github.com/brandonkounse/blog/db"
 )
 
-type Post struct {
-	Body string
-	Date time.Time
-}
+func Home(store *db.Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
-func Home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		tmpl, err := template.ParseFiles("index.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 
-	tmpl, err := template.ParseFiles("index.html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		post, err := store.GetNewestPost()
+		if err != nil {
+			if err == sql.ErrNoRows {
+				post = db.Post{
+					Content:  "No posts found!",
+					Category: "General",
+				}
+			} else {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+		}
+
+		tmpl.Execute(w, post)
 	}
-
-	post := Post{Body: "This is an example test body!"}
-
-	tmpl.Execute(w, post)
 }
